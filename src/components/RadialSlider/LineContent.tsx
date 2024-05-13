7a81c5b086891fc3167046f28367e43bf64e4c16
Sagar Pawar <sagar.pawar@simformsolutions.com>
2024-05-13 15:10:51 +0530

    feat: Changes according to custom UI of a project

--- src/components/RadialSlider/LineContent.tsx
+++ src/components/RadialSlider/LineContent.tsx
@@ -95,42 +95,95 @@ const LineContent = (props: LineContentProps) => {
           ? adjustedSliderStartPoint
           : 90;
 
-        return (
-          <G key={index.toString()}>
-            {(index % lineSpace === 0 ||
-              index === markIndex ||
-              isSpeedoMeterMarkerLine) && (
-              <G
-                transform={`translate(${
-                  radius + (lineHeight - thumbBorderWidth)
-                }, ${radius + (lineHeight - thumbBorderWidth)})`}
-              >
-                <Line
-                  x1={
-                    index === markIndex && !isHideMarkerLine
-                      ? radius + markerLineSize
-                      : radius + lineHeight - 12
-                  }
-                  x2={radius - 10 + lineHeight / 2}
-                  transform={`rotate(${
-                    index + radialCircleLineRotation + angle
-                  })`}
-                  strokeWidth={2}
-                  stroke={
-                    activeIndex > index ||
-                    (index === markIndex && !isHideMarkerLine)
-                      ? Platform.OS === "web"
-                        ? linearGradient[0].color
-                        : "url(#gradient)"
-                      : lineColor
-                  }
-                  fill="none"
-                  strokeLinecap="round"
-                />
-              </G>
-            )}
-          </G>
-        );
+          return (
+            <G>
+              {lines.map((_value, index) => {
+                const plusActiveIndex = index === 0 ? 0 : 1;
+                const activeIndex =
+                  (((((value as number) - min) * 100) / (max - min)) * lineCount) /
+                    100 +
+                  plusActiveIndex;
+        
+                const getMarketIndex = () => {
+                  return markerPositionValues.map((val: number) => {
+                    return Math.floor(
+                      ((((val - min) * 100) / (max - min)) * lineCount) / 100,
+                    );
+                  });
+                };
+        
+                const markIndex = Math.floor(
+                  (((((!fixedMarker ? (markerValue as number) : (value as number)) -
+                    min) *
+                    100) /
+                    (max - min)) *
+                    lineCount) /
+                    100,
+                );
+        
+                const isMarkerLine = getMarketIndex()?.includes(index);
+        
+                const isSpeedoMarker = !isMarkerVariant ? 0 : isMarkerLine ? -10 : 0;
+        
+                const isSpeedoMeterMarkerLine =
+                  isRadialCircleVariant || isRadialSliderVariant || isSpeedoMeterVariant
+                    ? false
+                    : isMarkerLine;
+        
+                // Calculate the slider point adjustment based on the provided start point
+                // The values -4 and -2 are used for fine-tuning the adjustment
+                // -4 accounts for a base adjustment, and -2 is a multiplier applied to the start point
+                const sliderPointAdjustment = -4 - 2 * (props?.startAngle || 0);
+        
+                // Adjust the slider start point based on variant type or use a default value (86)
+                const adjustedSliderStartPoint =
+                  isRadialCircleVariant && props?.startAngle != null
+                    ? props.startAngle + sliderPointAdjustment
+                    : 89;
+        
+                const radialCircleLineRotation = isRadialCircleVariant
+                  ? adjustedSliderStartPoint
+                  : 90;
+        
+                return (
+                  <G key={index.toString()}>
+                    {(index % lineSpace === 0 ||
+                      index === markIndex ||
+                      isSpeedoMeterMarkerLine) && (
+                      <G
+                        transform={`translate(${
+                          radius + (lineHeight - thumbBorderWidth)
+                        }, ${radius + (lineHeight - thumbBorderWidth)})`}
+                      >
+                        <Line
+                          x1={
+                            index === markIndex && !isHideMarkerLine
+                              ? radius + markerLineSize
+                              : radius + lineHeight - 12
+                          }
+                          x2={radius - 10 + lineHeight / 2}
+                          transform={`rotate(${
+                            index + radialCircleLineRotation + angle
+                          })`}
+                          strokeWidth={2}
+                          stroke={
+                            activeIndex > index ||
+                            (index === markIndex && !isHideMarkerLine)
+                              ? Platform.OS === "web"
+                                ? linearGradient[0].color
+                                : "url(#gradient)"
+                              : lineColor
+                          }
+                          fill="none"
+                          strokeLinecap="round"
+                        />
+                      </G>
+                    )}
+                  </G>
+                );
+              })}
+            </G>
+          );
       })}
     </G>
   );
--- src/components/RadialSlider/TailText.tsx
+++ src/components/RadialSlider/TailText.tsx
@@ -11,16 +11,22 @@ const TailText = (props: TextTailProps) => {
   return (
     <>
       <G transform={`translate(${-20}, ${40})`}>
-        <SVGText fill={Colors.darkCharcoal} fontSize={12}>
-          <TSpan x={startPoint.x} y={startPoint.y}>
-            {`${min} ${unit}`}
+        <SVGText fill={Colors.darkCharcoal} fontSize={16} fontWeight={"600"}>
+          <TSpan x={startPoint.x + 5} y={startPoint.y + 10}>
+            {`Not`}
+          </TSpan>
+          <TSpan x={startPoint.x} y={startPoint.y + 30}>
+            {`Likely`}
           </TSpan>
         </SVGText>
       </G>
       <G transform={`translate(${-10}, ${40})`}>
-        <SVGText fill={Colors.darkCharcoal} fontSize={12}>
-          <TSpan x={endPoint.x} y={endPoint.y}>
-            {`${max} ${unit}`}
+        <SVGText fill={Colors.darkCharcoal} fontSize={16} fontWeight={"600"}>
+          <TSpan x={endPoint.x - 30} y={endPoint.y + 10}>
+            {`Extremely`}
+          </TSpan>
+          <TSpan x={endPoint.x - 10} y={endPoint.y + 30}>
+            {`Likely`}
           </TSpan>
         </SVGText>
       </G>
